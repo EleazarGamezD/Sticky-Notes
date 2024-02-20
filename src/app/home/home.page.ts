69f3096f4e0ee156c96c589cfdcebd080c3ba4fb
Eleazar Gamez <82742317+EleazarGamezD@users.noreply.github.com>
2024-02-19 23:50:41 -0500

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private inAppBrowser: InAppBrowser) { }
+
+  ngOnInit(): void {
+    const browser = this.inAppBrowser.create('https://front-app-notas.vercel.app/',
+      '_self',
+      'location=no,hideurlbar=true, hidenavigationbuttons=true,zoom=no');
+    browser.on("exit").subscribe((evt) => {
+      App.exitApp();
+    })
+  }
 
 }
